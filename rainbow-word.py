fa605440f3cc75e3ab6feba4cd4dd1a63dcae281
Wouter Molhoek <woutermolhoek@hotmail.com>
2019-04-14 15:06:38 +0200

    Retrieve words from the file and store the in an array

--- rainbow-word.py
+++ rainbow-word.py
@@ -6,26 +6,33 @@ doc = Document('test-file.docx')
 # All the paragraphs stored in an array
 allTxt = doc.paragraphs
 
+
 # Function to delete a paragraph
 def delete_paragraph(paragraph):
     p = paragraph._element
     p.getparent().remove(p)
     p._p = p._element = None
 
+
+totalWords = []
+
 # Loop through the existing paragraphs, delete the old ones and replace them with the colorful ones.
 for p in allTxt:
     # Remove existing paragraphs
     delete_paragraph(p)
-    # Create new paragraph
-    run = doc.add_paragraph().add_run(p.text)
+    # Split every word between a space
+    words = p.text.split(' ')
+    # Push the words in the totalWords array
+    totalWords.append(words)
+
+for word in range(0, len(totalWords)):
+    run = doc.add_paragraph().add_run(totalWords[word])
     font = run.font
+
     # Set font color to red
     font.color.rgb = RGBColor(255, 0, 0)
-    # Set font color to orange
-    #font.color.rgb = RGBColor(255, 165, 0)
-    # Set font color to yellow
-    #font.color.rgb = RGBcolor(255, 215, 0)
 
+print(totalWords)
 
 # Create a new file and save it
 doc.save('demo1.docx')
