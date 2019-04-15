257e06114edd135939ff88021e55647d0be94864
Wouter Molhoek <woutermolhoek@hotmail.com>
2019-04-15 18:22:38 +0200

    Changed text color to the color of the rainbow

--- rainbow-word.py
+++ rainbow-word.py
@@ -15,6 +15,10 @@ def delete_paragraph(paragraph):
 
 
 totalWords = []
+newWordList = []
+
+# Red, Orange, Yellow, Green, Blue, Violet
+colors = [RGBColor(255, 0, 0), RGBColor(255, 117, 0), RGBColor(255, 249, 0), RGBColor(76, 223, 0), RGBColor(26, 103, 236), RGBColor(151, 23, 238)]
 
 # Loop through the existing paragraphs, delete the old ones and replace them with the colorful ones.
 for p in allTxt:
@@ -22,17 +26,33 @@ for p in allTxt:
     delete_paragraph(p)
     # Split every word between a space
     words = p.text.split(' ')
-    # Push the words in the totalWords array
-    totalWords.append(words)
 
-for word in range(0, len(totalWords)):
-    run = doc.add_paragraph().add_run(totalWords[word])
+    # Loop through the words an push them in the totalWords array
+    for word in words:
+        totalWords.append(word)
+
+part = int(len(totalWords) / 6)
+
+
+def add_words(begin, end, color):
+    for i in range(begin, end):
+        newWordList.append(totalWords[i])
+
+    run = doc.add_paragraph().add_run(newWordList)
     font = run.font
 
     # Set font color to red
-    font.color.rgb = RGBColor(255, 0, 0)
+    font.color.rgb = color
+    # Clear the previous array
+    newWordList.clear()
 
-print(totalWords)
+
+add_words(0, part, colors[0])
+add_words(part, part * 2, colors[1])
+add_words(part * 2, part * 3, colors[2])
+add_words(part * 3, part * 4, colors[3])
+add_words(part * 4, part * 5, colors[4])
+add_words(part * 5, part * 6, colors[5])
 
 # Create a new file and save it
-doc.save('demo1.docx')
+doc.save('rainbow.docx')
